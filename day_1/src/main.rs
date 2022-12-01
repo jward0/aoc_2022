dc0d664bef25de17363b86333985c0e34f7aec12
James Ward <jward11235@gmail.com>
2022-12-01 09:46:33 +0000

    day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Error, Read},
+};
+
+
+fn sorted_inputs_from_file<R: Read>(io: R) -> Result<Vec<Vec<i64>>, Error> {
+    let br = BufReader::new(io);
+    let mut output_vec = vec![];
+    let new_empty_vec: Vec<i64> = vec![];
+    output_vec.push(new_empty_vec);
+    for line in br.lines() {
+        let line_contents = line?;
+        if line_contents.is_empty() {
+            let new_empty_vec: Vec<i64> = vec![];
+            output_vec.push(new_empty_vec);
+        }
+        else {
+            output_vec.last_mut().unwrap().push(line_contents.parse::<i64>().unwrap());
+        }
+    }
+    Ok(output_vec)
+}
+
+
+fn part_1(input:&Vec<Vec<i64>>) -> Result<i64, Error> {
+    let summed_inputs: Vec<i64> = input.into_iter().map(|v| v.into_iter().sum()).collect();
+    let max_value: i64 = *summed_inputs.iter().max().unwrap();
+    Ok(max_value)
+} 
+
+
+fn part_2(input:&Vec<Vec<i64>>) -> Result<i64, Error> {
+    let mut summed_inputs: Vec<i64> = input.into_iter().map(|v| v.into_iter().sum()).collect();
+    summed_inputs.sort();
+    summed_inputs = summed_inputs.into_iter().rev().collect();
+    Ok(summed_inputs[0] + summed_inputs[1] + summed_inputs[2])
+}
+
+
+fn main() -> Result<(), Error> {
+    let input = sorted_inputs_from_file(File::open("input.txt")?)?;
+    println!("{}", part_1(&input)?);
+    println!("{}", part_2(&input)?);
+    Ok(())
 }
